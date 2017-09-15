9b5ee54e561dcc1e8c41055690c67518f42788e2
unknown <yaronefrat92@gmail.com>
2017-09-15 18:03:26 +0300

    Python update: character list to dict.

--- Char_Crea.py
+++ Char_Crea.py
@@ -27,38 +27,48 @@ def insert_dat(legal,index,message):
 #The "social" general status of the character[2]
 #and The economical if the character is urban or rural
 # (Noble and Tribal are assigned automatically)[3]
-character=[0,'0','0','0'];
+character = {'Name': 0, 'Race': '0', 'Soc': '0', 'Eco': '0'};
 
 print("Choose Your name (maximum 24 characters)");
-character[0]=input(); #Getting the name from the user
-print(character[0]); #TEST printing the name
-print("Hello {name}. How are you".format(name=character[0])); #TEST printing the name
+character['Name']=input(); #Getting the name from the user
+print("The character's name is:", character['Name']); #TEST printing the name
+#print("Hello {name}. How are you".format(name=character[0])); #TEST printing the name
 
-#Prompting the user to choos his race
+#Prompting the user to choose his race
 print("Choose Your race amongst the following:");
-legal=['H','h','E','e','D','d','G','g','B','b','C']; #Creating a list of legal input for the races so that it can easily be checked whether the input is legal
+legal = ['H', 'h', 'E', 'e', 'D', 'd', 'G', 'g', 'B', 'b', 'C']; #Creating a list of legal input for the races so that it can easily be checked whether the input is legal
 #Will keep asking the user for a race as long as he didn't put a legal input. Using the insert_dat function
-insert_dat(legal,1,"(H)uman, (E)lf, (D)warf, (G)nome, (B)asil, (C)P");
-print(character[1]); #TEST printing the characer's race
+insert_dat(legal,'Race',"(H)uman, (E)lf, (D)warf, (G)nome, (B)asil, (C)P");
+print("The character's race is:", character['Race']); #TEST printing the characer's race
 legal=['N','n','U','u','R','r','T','t']; #Creating a list of legal input for soc so that it can easily be checked whether the input is legal
 print("Choose Your background amongst the following:");
 #Prompting the user to choose a soc using the letters in brackets.
 #The list of legal inputs has changed
-insert_dat(legal,2,"(N)oble, (U)rban, (R)ural, (T)ribal (or from a small community)");
-print(character[2]); #TEST printing the characer's soc
-if (character[2]=='N' or character[2]=='n'):
-    character[3]='h'; #Giving Noble characters the "higher class" economical status
+insert_dat(legal,'Soc',"(N)oble, (U)rban, (R)ural, (T)ribal (or from a small community)");
+print("The character's soc is:", character['Soc']); #TEST printing the characer's soc
+if (character['Soc']=='N' or character['Soc']=='n'):
+    character['Eco']='h'; #Giving Noble characters the "higher class" economical status
     print("Nobles are automatically rich (at least for now)");
-elif (character[2]=='T' or character[2]=='t'):
-    character[3] = 'l'; #Giving Tribal characters the "lower class" economical status
+elif (character['Soc']=='T' or character['Soc']=='t'):
+    character['Eco'] = 'l'; #Giving Tribal characters the "lower class" economical status
     print("Tribals are automatically from the lower economical class");
 else:
     legal = ['H', 'h', 'M', 'm', 'L', 'l', 'S','s'];  # Creating a list of legal input for eco so that it can easily be checked whether the input is legal
     print("Choose Your background amongst the following:");
     # Prompting the user to choose a eco IF he chose urban or rural using the letters in brackets.
     # The list of legal inputs has changed
-    insert_dat(legal, 3, "(H)igher class, (M)iddle class, (L)ower class, (S)treet urchin");
-print(character[3]);  # TEST printing the characer's soc
+    insert_dat(legal, 'Eco', "(H)igher class, (M)iddle class, (L)ower class, (S)treet urchin");
+print("The character's eco is:", character['Eco']);  # TEST printing the characer's eco
+
+#Update log:
+#15.9.2017, 18:01 - Changed "character" from a list to a dictionary
+#and added extra "padding" to the messages showing the chosen input
+
+#NEED TO ADD LATER:
+#Writing a character to a file,
+#Adding another Program which can read a character from a file
+#Adding a program which the other program can use, containing all sort of functions,
+# like getting a character's stats, changing them etc.
 
 
 
